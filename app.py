8d09dd9c81a51d2bd3d007cfcae999ba61d5b6d2
RitamPaul <ritampaulblogpost@gmail.com>
2024-08-14 19:00:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,35 +17,35 @@ st.markdown(
 
 # Login as Admin / User / New User
 uid, pwd = "", ""
-btn1, btn2, btn3, submit = False, False, False, False
+btnA, btnU, btnNU, submit = False, False, False, False
 col1, col2, col3 = st.columns(3, vertical_alignment="center")
 with col1:
     container1 = st.container(border=False)
-    btn1 = container1.button("Log in as ADMIN", type='primary', use_container_width=True)
+    btnA = container1.button("Log in as ADMIN", type='primary', use_container_width=True)
 with col2:
     container2 = st.container(border=False)
-    btn2 = container2.button("Log in as USER", type='primary', use_container_width=True)
+    btnU = container2.button("Log in as USER", type='primary', use_container_width=True)
 with col3:
     container3 = st.container(border=False)
-    btn3 = container3.button("Create account", type='primary', use_container_width=True)
+    btnNU = container3.button("Create account", type='primary', use_container_width=True)
 
-while(not submit):
-    if(btn1 or btn2):
-        uid = st.text_input(
-            label="**Admin ID**"*btn1 or "**User ID**"*btn2,
-            placeholder="admin id"*btn1 or "user id"*btn2
-        )
-        pwd = st.text_input(label="**Password**", placeholder="password", type="password")
-        with st.columns([0.25,0.5,0.25])[1]:
-            submit = st.button("submit", type='primary', use_container_width=True)
+if(btnA or btnU):
+    uid = st.text_input(
+        label="**Admin ID**"*btnA or "**User ID**"*btnU,
+        placeholder="admin id"*btnA or "user id"*btnU,
+        key="uidold"
+    )
+    pwd = st.text_input(label="**Password**", placeholder="password", key="pwdold", type="password")
+    with st.columns([0.25,0.5,0.25])[1]:
+        submit = st.button("submit", type='primary', use_container_width=True)
 
-    elif(btn3):
-        name = st.text_input(label="**Full name**", placeholder="full name")
-        eid = st.text_input(label="**Unique employee ID**", placeholder="unique employee id")
-        uid = st.text_input(label="**Create your login ID**", placeholder="id")
-        pwd = st.text_input(label="**Create your Password**", placeholder="password", type="password")
-        with st.columns([0.25,0.5,0.25])[1]:
-            submit = st.button("submit", type='primary', use_container_width=True)
+elif(btnNU):
+    name = st.text_input(label="**Full name**", placeholder="full name", key="namenew")
+    eid = st.text_input(label="**Unique employee ID**", placeholder="unique employee id", key="eidnew")
+    uid = st.text_input(label="**Create your login ID**", placeholder="id", key="uidnew")
+    pwd = st.text_input(label="**Create your Password**", placeholder="password", type="password", key="pwdnew")
+    with st.columns([0.25,0.5,0.25])[1]:
+        submit = st.button("Register", type='primary', use_container_width=True)
 
 
 # # Path for any image to display
